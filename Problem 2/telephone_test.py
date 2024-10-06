9e53d42a567e6a68d6f74507026859d2d514c45a
Jacob C. Hart <37990467+drdauntless@users.noreply.github.com>
2024-10-06 14:54:04 -0500

    updated test cases

--- Problem 2/telephone_test.py
+++ Problem 2/telephone_test.py
@@ -3,30 +3,29 @@ Test cases for the telephone module.
 '''
 import telephone as telephone
 
-class TestClass:
-    def test_one(self,monkeypatch,capsys):
+def test_two_one(monkeypatch,capsys):
 
-        # monkeypatch the "input" function, so that it returns "Mark".
-        # This simulates the user entering "Mark" in the terminal:
-        inputs = iter(["8005551212"])
-        monkeypatch.setattr('builtins.input', lambda: next(inputs, '\n'))
+    # monkeypatch the "input" function, so that it returns "Mark".
+    # This simulates the user entering "Mark" in the terminal:
+    inputs = iter(["8005551212"])
+    monkeypatch.setattr('builtins.input', lambda: next(inputs, '\n'))
 
-        # go about using input() like you normally would:
-        telephone.telephone()
-        captured = capsys.readouterr()
-        all_outputs = captured.out.split('\n')
+    # go about using input() like you normally would:
+    telephone.telephone()
+    captured = capsys.readouterr()
+    all_outputs = captured.out.split('\n')
 
-        assert "(800) 555-1212" in all_outputs, "Make sure that the number is formatted correctly"
+    assert "(800) 555-1212" in all_outputs, "Make sure that the number is formatted correctly"
 
-    def test_two(self,monkeypatch,capsys):
+def test_two_two(monkeypatch,capsys):
 
-        # monkeypatch the "input" function, so that it returns "Mark".
-        # This simulates the user entering "Mark" in the terminal:
-        inputs = iter(["9995551111"])
-        monkeypatch.setattr('builtins.input', lambda: next(inputs, '\n'))
+    # monkeypatch the "input" function, so that it returns "Mark".
+    # This simulates the user entering "Mark" in the terminal:
+    inputs = iter(["9995551111"])
+    monkeypatch.setattr('builtins.input', lambda: next(inputs, '\n'))
 
-        # go about using input() like you normally would:
-        telephone.telephone()
-        captured = capsys.readouterr()
-        all_outputs = captured.out.split('\n')
-        assert "(999) 555-1111" in all_outputs, "Make sure that the number is formatted correctly"
+    # go about using input() like you normally would:
+    telephone.telephone()
+    captured = capsys.readouterr()
+    all_outputs = captured.out.split('\n')
+    assert "(999) 555-1111" in all_outputs, "Make sure that the number is formatted correctly"
